5f5fe03bb6b6c92f8b212c488e100df5d3615f5b
Felipe Tulio <eng.felipetulio@gmail.com>
2023-09-30 19:33:50 -0300

    Tipos primitivos em JS

---
+++ js-base/tiposPrimitivos.js
@@ -0,0 +1,16 @@
+// Tipos primitivos de variáveis em JS
+
+const meuNumero = 3;
+
+const primeiroNumero = 1
+const segundoNumero = 2
+
+const op = primeiroNumero + segundoNumero
+
+console.log("o resultado da operação é " + op.toString())
+
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+
+console.log(texto1)
+console.log(texto2)
