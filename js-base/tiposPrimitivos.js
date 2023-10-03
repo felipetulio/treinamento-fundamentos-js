50c87969c83bfdd5ca4157ac03dcbd63a18d61f7
Felipe Tulio <eng.felipetulio@gmail.com>
2023-10-03 14:31:58 -0300

    Adicionando comentários no código

--- js-base/tiposPrimitivos.js
+++ js-base/tiposPrimitivos.js
@@ -1,14 +1,23 @@
-// Tipos primitivos de variáveis em JS
+// Tipos primitivos de variáveis em JS -> números (inteiros e flutuantes), 
+// palavras ou texto (Strings) e booleanos.
 
+// A tipagem da linguagem é dinâmica. 
 const meuNumero = 3;
-
 const primeiroNumero = 1
 const segundoNumero = 2
 
 const op = primeiroNumero + segundoNumero
 
+// primeira forma de casting para impressão de resultados no console. Método tradicional 
+// usando o operador + como mecanismo de concatenação. 
 console.log("o resultado da operação é " + op.toString())
 
+// segunda forma de casting para impressão de resultados no console. Método alternativo do
+// JS chamado de interpolação. É mais prático para formar sentenças de saída com diversas
+// variáveis
+console.log(`O resultado da operação é ${op}`)
+
+// Strings podem ser escritas com aspas duplas ou simples
 const texto1 = "Olá, mundo!";
 const texto2 = 'Olá, mundo!';
 
